13f47a1c763c299465b8f53eed9203981a05ba3b
rafaeldss <rafaeldss199916@gmail.com>
2020-05-10 18:40:44 -0300

    Modificação do arquivo wsgi.py para facilitar a execução da aplicação locamente ou no servidor sem precisar trocar as configurações

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,8 @@ from app import create_app
 from app.ext.websocket import socketio
 
 
-app = application = create_app('config.Config')
+app = application = create_app('config.ProductionConfig')
 
 if __name__ == '__main__':
+    app = create_app()
     socketio.run(app)
